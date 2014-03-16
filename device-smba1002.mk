99c81559a9fb62c60c5a8ab96c3a71a8e1c065c5
Boris Popov <bvpopov@gmail.com>
2014-03-16 02:40:26 -0700

    device-smba1002: vold.fstab depricated

--- device-smba1002.mk
+++ device-smba1002.mk
@@ -51,7 +51,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Harmony Configs
 PRODUCT_COPY_FILES += \
 	device/malata/smba1002/prebuilt/ramdisk/init.harmony.rc:root/init.harmony.rc \
-	device/malata/smba1002/prebuilt/vold.fstab:system/etc/vold.fstab \
 	device/malata/smba1002/prebuilt/ramdisk/fstab.harmony:root/fstab.harmony \
 	device/malata/smba1002/prebuilt/03sysctl:system/etc/init.d/03sysctl \
 	device/malata/smba1002/prebuilt/ntfs-3g:system/xbin/ntfs-3g
